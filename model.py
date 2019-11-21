561d437d4a235b554b50e00e896bf2e5c896152b
cnalty <cnaltypga@gmail.com>
2019-11-21 15:57:28 -0500

    changed model

--- model.py
+++ model.py
@@ -9,33 +9,43 @@ class CharNet(nn.Module):
         self.conv1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
         self.norm1 = nn.BatchNorm2d(8)
         self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
-        self.pool1 = nn.MaxPool2d(2)
-        self.conv3 = nn.Conv2d(16, 16, kernel_size=3, padding=1)
         self.norm2 = nn.BatchNorm2d(16)
-        self.conv4 = nn.Conv2d(16, 16, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(16, 16, kernel_size=3, padding=1)
+        self.norm3 = nn.BatchNorm2d(16)
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv4 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.norm4 = nn.BatchNorm2d(32)
+        self.conv5 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.norm5 = nn.BatchNorm2d(32)
         self.pool2 = nn.MaxPool2d(2)
 
-
-
-        self.fc1 = nn.Linear(784, out_chars)
+        self.fc1 = nn.Linear(1568, 1568)
+        self.fc2 = nn.Linear(1568, out_chars)
 
     def forward(self, x):
         out = self.conv1(x)
         out = self.norm1(out)
         out = F.relu(out)       
         out = self.conv2(out)
+        out = self.norm2(out)
+        out = self.conv3(out)
+        out = self.norm3(out)
+        out = F.relu(out)
         out = F.relu(out)
         out = self.pool1(out)
-        out = self.conv3(out)
-        out = self.norm2(out)
-        out = F.relu(out)      
         out = self.conv4(out)
+        out = self.norm4(out)
+        out = F.relu(out)
+        out = self.conv5(out)
+        out = self.norm5(out)
         out = F.relu(out)
         out = self.pool2(out)
 
         out = out.view(out.size(0), -1)
 
         out = self.fc1(out)
+        out = F.relu(out)
+        out = self.fc2(out)
 
         return out
 
