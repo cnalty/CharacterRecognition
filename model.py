e497c56d024641bce1ad577bf66f8d1583b86ba2
cnalty <cnaltypga@gmail.com>
2019-11-21 20:34:18 -0500

    added more filters to layers

--- model.py
+++ model.py
@@ -6,21 +6,21 @@ class CharNet(nn.Module):
     def __init__(self, out_chars):
         super(CharNet, self).__init__()
 
-        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
-        self.norm1 = nn.BatchNorm2d(8)
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
+        self.norm1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 16, kernel_size=3, padding=1)
         self.norm2 = nn.BatchNorm2d(16)
-        self.conv3 = nn.Conv2d(16, 16, kernel_size=3, padding=1)
-        self.norm3 = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.norm3 = nn.BatchNorm2d(32)
         self.pool1 = nn.MaxPool2d(2)
-        self.conv4 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.norm4 = nn.BatchNorm2d(32)
-        self.conv5 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
-        self.norm5 = nn.BatchNorm2d(32)
+        self.conv4 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.norm4 = nn.BatchNorm2d(64)
+        self.conv5 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.norm5 = nn.BatchNorm2d(64)
         self.pool2 = nn.MaxPool2d(2)
 
-        self.fc1 = nn.Linear(1568, 1568)
-        self.fc2 = nn.Linear(1568, out_chars)
+        self.fc1 = nn.Linear(3136, 3136)
+        self.fc2 = nn.Linear(3136, out_chars)
 
     def forward(self, x):
         out = self.conv1(x)
